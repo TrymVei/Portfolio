a3fe71022fa9638e41ff2725ae3032628fc37f27
TrymVei <tve@variant.no>
2024-03-05 21:37:06 +0100

    feat(nav): simple nav

---
+++ src/app/components/atoms/navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-darkViolet h-16 px-16 flex items-center">
+      <ul className="flex gap-6 *:text-xl">
+        <li>
+          <a href="#home">Hjem</a>
+        </li>
+        <li>
+          <a href="#about">Om meg</a>
+        </li>
+        <li>
+          <a href="#projects">Prosjekter</a>
+        </li>
+        <li>
+          <a href="#contact">Kontakt</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
