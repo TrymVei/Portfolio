d85ed38dcae22322b868f3ed98d3d935ca838dac
TrymVei <tve@variant.no>
2024-03-21 10:34:52 +0100

    feat: added heading and paragraph for projects

--- src/app/components/atoms/projects.tsx
+++ src/app/components/atoms/projects.tsx
@@ -4,6 +4,10 @@ import { ProjectCard } from "../molecules/projectCard";
 export const Projects = () => {
   return (
     <div className="bg-LightGrayishCyan px-16 pb-16">
+      <h2 className="text-russianViolet">Prosjekter</h2>
+      <p className="text-russianViolet text-xl my-2">
+        Her er noen av prosjektene jeg har jobbet på i løpet av min karriere
+      </p>
       <ProjectCard
         title={"Variant Håndbok"}
         image={"/img/handbook.png"}
